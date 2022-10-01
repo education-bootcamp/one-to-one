1e93c290aea0d5bec7fb9f638644e9e888c43bd7
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-10-01 10:37:46 +0530

    4

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,2 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtCustomerId;
+    public TextField txtCustomerName;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerSalary;
+    public TableView tblCustomer;
+    public TableColumn colCustomerId;
+    public TableColumn colCustomerAddress;
+    public TableColumn colCustomerName;
+    public TableColumn colCustomerSalary;
+
+    public void initialize(){
+        loadCustomers();
+    }
+
+    private void loadCustomers() {
+
+    }
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+    }
 }
